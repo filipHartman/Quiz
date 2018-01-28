cc0e75f0bb07f0cf40876f287838c5a8e076834b
filipHartman <filip.hartman23@gmail.com>
2018-01-28 12:07:31 +0100

    Add getters and setters for question model fields

--- src/model/QuestionModel.java
+++ src/model/QuestionModel.java
@@ -7,4 +7,36 @@ public class QuestionModel {
     private String questionContent;
     private HashMap<char, String> answers;
     private char correctAnswerIndex;
+
+    public void setQuestionType(String questionType) {
+        this.questionType = questionType;
+    }
+
+    public String getQuestionType() {
+        return questionType;
+    }
+
+    public String getQuestionContent() {
+        return questionContent;
+    }
+
+    public void setQuestionContent(String questionContent) {
+        this.questionContent = questionContent;
+    }
+
+    public HashMap<char, String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(HashMap<char, String> answers) {
+        this.answers = answers;
+    }
+
+    public char getCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
+
+    public void setCorrectAnswerIndex(char correctAnswerIndex) {
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
 }
