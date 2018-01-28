8a4f9fe9c14af14acb34d9e9bc59ff109b58525f
filipHartman <filip.hartman23@gmail.com>
2018-01-28 20:07:24 +0100

    Create controller responsible for menu of app

---
+++ src/controller/MenuController.java
@@ -0,0 +1,21 @@
+package controller;
+
+public class MenuController {
+    private MenuView menuView = new MenuView();
+
+    public void startGame() {
+
+    }
+
+    public void displayMenu() {
+
+    }
+
+    public void handleUserOption(String option) {
+
+    }
+
+    public void displayHighscore() {
+
+    }
+}
