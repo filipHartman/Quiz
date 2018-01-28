072ef014936388b37c2b91f851fd7144dbd79ea7
filipHartman <filip.hartman23@gmail.com>
2018-01-28 21:13:15 +0100

    Add creating menu, display text and getting input

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -1,16 +1,29 @@
 package view;
 
 import java.util.HashMap;
+import java.util.Scanner;
+
 
 public class MenuView {
-    private HashMap<Integer, String> mainMenu;
+    private Scanner scanner = new Scanner(System.in);
+    private HashMap<Integer, String> mainMenu = createMainMenu();
 
-    public displayText(String text) {
-
+    private HashMap<Integer,String> createMainMenu() {
+        HashMap<Integer,String> mainMenu = new HashMap<Integer, String>();
+        mainMenu.put(1, "Start game");
+        mainMenu.put(2, "Create question");
+        mainMenu.put(3, "Display highscore");
+        mainMenu.put(0, "Exit");
+        return mainMenu;
     }
 
-    public String getUserInput() {
-        return "";
+    public void displayText(String text) {
+        System.out.println(text);
+    }
+
+    public String getUserInput(String request) {
+        System.out.print(request);
+        return scanner.next();
     }
 
     public void dispalyMenu() {
