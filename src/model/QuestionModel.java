7ef3d1bd7b600df1845425964724aad97c89b2d2
filipHartman <filip.hartman23@gmail.com>
2018-01-28 12:03:13 +0100

    Add fields for question model

---
+++ src/model/QuestionModel.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.HashMap;
+
+public class QuestionModel {
+    private String questionType;
+    private String questionContent;
+    private HashMap<char, String> answers;
+    private char correctAnswerIndex;
+}
