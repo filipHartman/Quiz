c98d8c2e4200b1fe023e7930b6f1775149065985
filipHartman <filip.hartman23@gmail.com>
2018-01-28 12:11:01 +0100

    Add quiz model class implementation

---
+++ src/model/QuizModel.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.ArrayList;
+
+public class QuizModel {
+    private ArrayList<QuizModel> questions;
+
+    public void setQuestions(ArrayList<QuizModel> questions) {
+        this.questions = questions;
+    }
+
+    public ArrayList<QuizModel> getQuestions() {
+        return questions;
+    }
+}
