9fa059e1d1301c17ddd02b48decb119e02bd6469
filipHartman <filip.hartman23@gmail.com>
2018-01-28 20:08:48 +0100

    Add file responsible for view of menu

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -1,4 +1,19 @@
 package view;
 
+import java.util.HashMap;
+
 public class MenuView {
+    private HashMap<Integer, String> mainMenu;
+
+    public displayText(String text) {
+
+    }
+
+    public String getUserInput() {
+        return "";
+    }
+
+    public void dispalyMenu() {
+
+    }
 }
