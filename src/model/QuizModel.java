5e92b1f3ce02fbe2c75fb94b703ecbfecc3b9d0c
filipHartman <filip.hartman23@gmail.com>
2018-01-29 23:40:14 +0100

    Add constructor to class

--- src/model/QuizModel.java
+++ src/model/QuizModel.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 public class QuizModel {
     private ArrayList<QuizModel> questions;
 
+    public QuizModel(ArrayList<QuizModel> questions) {
+        this.questions = questions;
+    }
+
     public void setQuestions(ArrayList<QuizModel> questions) {
         this.questions = questions;
     }
