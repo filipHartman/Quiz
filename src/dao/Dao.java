96b48540430733cd589a8e592e420c4a3487575b
filipHartman <filip.hartman23@gmail.com>
2018-01-30 21:20:24 +0100

    Add connecting to db

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -1,4 +1,17 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Dao {
+    private Connection connection;
+
+    public void connectToDatabase() {
+        try {
+                connection = DbConnection.getConnection());
+        } catch (ClassNotFoundException| SQLException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+    }
 }
