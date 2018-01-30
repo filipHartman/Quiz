2b63b3b8ace1c56bac8a299e47ba335452dd3a51
filipHartman <filip.hartman23@gmail.com>
2018-01-30 21:44:13 +0100

    Add importing question categories from db

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -1,17 +1,39 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Dao {
     private Connection connection;
 
     public void connectToDatabase() {
         try {
-                connection = DbConnection.getConnection());
-        } catch (ClassNotFoundException| SQLException e) {
+            connection = DbConnection.getConnection();
+        } catch (ClassNotFoundException e) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             System.exit(0);
         }
+    }
+
+    public ArrayList<String> importQuestionTypes() {
+        ArrayList<String> typeNamesCollection = new ArrayList<String>();
+        PreparedStatement ps = null;
+        String query = "SELECT * FROM types";
+        try {
+            ps = connection.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                String typeName = rs.getString("type_name");
+                typeNamesCollection.add(typeName);
+            }
+        } catch (SQLException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        return typeNamesCollection;
     }
 }
